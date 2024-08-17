08abd1df88530d6e6e001350bbc56df587860de2
Arsalan277 <140528410+Arsalan277@users.noreply.github.com>
2024-08-17 16:47:09 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t arsalan96/my-repo:jma-1.2 .'
+        sh 'docker build -t arsalan96/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push arsalan96/my-repo:jma-1.2'
+        sh 'docker push arsalan96/my-repo:jma-2.0'
     }
 } 
 
